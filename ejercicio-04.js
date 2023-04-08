fed67a892fdf75cb3e14aa30a87b67562e90f9e0
victor <vmvg08@hotmail.com>
2023-04-08 00:59:42 -0500

    Correción al ejercicio

--- ejercicio-04.js
+++ ejercicio-04.js
@@ -1,6 +1,6 @@
 let nombre = "Victor";
 let apellido = "Vergara";
-let estudiante =`estudiante ${nombre} ${apellido}` 
+let estudiante =`${nombre} ${apellido}` 
 let estudianteMayus = estudiante.toUpperCase();
 let estudianteMinus = estudiante.toLowerCase();
 let cantLetras = estudiante.length; 
@@ -9,4 +9,3 @@ let ultimaLetraApellido = apellido.charAt(apellido.length -1);
 let estudianteSinEspacios = estudiante.replace(/\s+/g, "");
 let nombreEnEstudiante = estudiante.includes(nombre);
 
-console.log(nombreEnEstudiante);
